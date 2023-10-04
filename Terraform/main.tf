71db7a31b8764cb3421ba0ba4d618cf94837322b
Sam <8408317+samgabrail@users.noreply.github.com>
2023-10-04 16:02:08 +0000

    tf files and devcontainer

---
+++ Terraform/main.tf
@@ -0,0 +1,13 @@
+module "eks" {
+  source  = "app.terraform.io/TeKanAid/eks/aws"
+  version = "0.0.7"
+
+  region               = "us-east-1"
+  cluster_version      = "1.27"
+  cluster_name         = "env0_eks_cluster"
+  instance_types       = ["t2.small"]
+  vpc_cidr             = "10.0.0.0/16"
+  cluster_min_size     = 1
+  cluster_max_size     = 3
+  cluster_desired_size = 2
+}
